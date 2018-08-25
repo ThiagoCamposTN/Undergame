94e729648ac0d8f7800dc1641dca2f710e5beec3
ThiagoCamposTN <thiagocampostn@gmail.com>
2018-08-25 19:48:12 -0300

    Fixing a warning, where the file is not being closed after opened.

--- engine/core/utils.py
+++ engine/core/utils.py
@@ -1,5 +1,9 @@
 import json
-import os
 
 def get_file_data(file_path):
-    return json.load(open(file_path))
+    json_data = None
+
+    with open(file_path) as opened_file:
+        json_data = json.load(opened_file)
+
+    return json_data
